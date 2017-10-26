c054c4bc3d7585c3daae3a3dd14e6dba3a631fde
Chidiebere <Chidiebere@Chidi>
2017-10-26 15:25:47 +0100

    Read the .txt file. The info there would be used when creating the database

---
+++ ModualrProject/src/database/managers/ConnectionManager.java
@@ -0,0 +1,14 @@
+
+package database.managers;
+
+/**
+ * This class contains static methods that would be used to establish 
+ * a connection to the MySql database
+ * 
+ * @author Oguejiofor Chidiebere
+ *
+ */
+public class ConnectionManager
+{
+   
+}
